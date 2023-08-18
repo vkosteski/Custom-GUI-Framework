5761dbb541cf0ed33bdb12750462657517120b8b
vkosteski <kosteskiviktor@gmail.com>
2023-08-18 12:00:00 +0100

    Add project files.

---
+++ backends/imgui_impl_win32.h
@@ -0,0 +1,26 @@
+
+
+#pragma once
+#include "imgui.h"      // IMGUI_IMPL_API
+
+IMGUI_IMPL_API bool     ImGui_ImplWin32_Init(void* hwnd);
+IMGUI_IMPL_API bool     ImGui_ImplWin32_InitForOpenGL(void* hwnd);
+IMGUI_IMPL_API void     ImGui_ImplWin32_Shutdown();
+IMGUI_IMPL_API void     ImGui_ImplWin32_NewFrame();
+
+// Win32 message handler your application need to call.
+// - Intentionally commented out in a '#if 0' block to avoid dragging dependencies on <windows.h> from this helper.
+// - You should COPY the line below into your .cpp code to forward declare the function and then you can call it.
+// - Call from your application's message handler. Keep calling your message handler unless this function returns TRUE.
+
+#if 0
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+#endif
+
+
+IMGUI_IMPL_API void     ImGui_ImplWin32_EnableDpiAwareness();
+IMGUI_IMPL_API float    ImGui_ImplWin32_GetDpiScaleForHwnd(void* hwnd);       // HWND hwnd
+IMGUI_IMPL_API float    ImGui_ImplWin32_GetDpiScaleForMonitor(void* monitor); // HMONITOR monitor
+
+
+IMGUI_IMPL_API void     ImGui_ImplWin32_EnableAlphaCompositing(void* hwnd);   // HWND hwnd
---
+++ examples/example_win32_directx9/blur_x.hpp
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include <cstdio>
+#include <array>
+#include <cstddef>
+
+template<std::size_t N>
+constexpr auto DecodeBase85(const std::array<char, N>& input) noexcept {
+    constexpr std::size_t OUTPUT_SIZE = (N - 1) * 4 / 5;
+    std::array<char, OUTPUT_SIZE> out{};
+
+    constexpr auto decode85Byte = [](char c) constexpr -> unsigned int {
+        return c >= '\\' ? c - 36 : c - 35;
+        };
+
+    for (std::size_t i = 0, j = 0; i + 4 < N; i += 5, j += 4) {
+        unsigned int tmp = decode85Byte(input[i]) +
+            85 * (decode85Byte(input[i + 1]) +
+                85 * (decode85Byte(input[i + 2]) +
+                    85 * (decode85Byte(input[i + 3]) +
+                        85 * decode85Byte(input[i + 4]))));
+
+        out[j] = static_cast<char>((tmp >> 0) & 0xFF);
+        out[j + 1] = static_cast<char>((tmp >> 8) & 0xFF);
+        out[j + 2] = static_cast<char>((tmp >> 16) & 0xFF);
+        out[j + 3] = static_cast<char>((tmp >> 24) & 0xFF);
+    }
+
+    return out;
+}
+
+// ✅ Define the encoded data as a `std::array<char, N>`, NOT a raw string
+constexpr std::array<char, 400> encodedData = {
+    "%/P:vP>$(#>T$<8?####Q$###%/P:v%####?####$&###J$###h####&####$####t#########.$###%####$####:$########tn=j=$8HlEQ2TuY3l:$#%),##$#########0g'WC"
+    "`-.:CVjSuY&5>##%),##$#########C,.33UnSc;'AViF6JrEH<Sk:0+bVe=K&&PDlf1eGdfX1F$*fUCs'el1K>,C5AH3I3b48(#$QUV$)%XHVd;#K7#####X/'.7`7r'7$QUV$*%XHV"
+    "..."
+};
+
+// ✅ Pass `encodedData` to `DecodeBase85
+//inline constexpr auto blur_x = DecodeBase85(encodedData);
